de3af8ff39138ee8d3dcc726d51ac903ceea2f3a
Honey-Sharma-git <Honeysharmaemail@gmail.com>
2024-11-17 10:17:29 +0530

    Separated script from HTML and created CSS file

---
+++ JS/multiplication-tables.js
@@ -0,0 +1,28 @@
+function generateTable() {
+    getNumber();
+    doMultiplication();
+    displayTable();
+  }
+  let number = 0;
+  function getNumber() {
+    number = Number(document.querySelector(".js-input").value);
+  }
+  let product = 0;
+  let i;
+  let multiplicationTable = [];
+  function doMultiplication() {
+    for (i = 1; i <= 10; i++) {
+      product = number * i;
+      let productLine = `${number} x ${i} = ${product}`;
+      multiplicationTable.push(productLine);
+    }
+  }
+  let tableSet = "";
+  function displayTable() {
+    for (let i = 0; i < multiplicationTable.length; i++) {
+      let productPara = multiplicationTable[i];
+      let html = `<p>${productPara}</p>`;
+      tableSet += html;
+      document.querySelector(".js-output").innerHTML = tableSet;
+    }
+  }
